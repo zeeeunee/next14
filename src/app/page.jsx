70722587de6efaf1c62f36bf87caad5004362c9c
zeeeunee <wldmstv@gmail.com>
2024-01-15 10:53:46 +0900

    init

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import clsx from 'clsx';
+import styles from './main.module.scss';
+
+export default function Home() {
+	return (
+		<main className={clsx(styles.main)}>
+			<h1>메인페이지</h1>
+		</main>
+	);
+}
