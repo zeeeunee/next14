3e53f41e4f4ca7a41e4a1b41938842f7222d02c9
zeeeunee <wldmstv@gmail.com>
2024-01-15 16:53:41 +0900

    브라우저 폭에 따라 header의 menu보이고 안보이게 처리

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -3,6 +3,7 @@ import styles from './header.module.scss';
 import Link from 'next/link';
 import Navbar from '../navbar/Navbar';
 import BtnLogin from '../btnLogin/BtnLogin';
+import { TiThMenu } from 'react-icons/ti';
 
 export default function Header() {
 	console.log('header');
@@ -14,6 +15,7 @@ export default function Header() {
 
 			<Navbar textArr={['about', 'youtube', 'post']} />
 			<BtnLogin session={false} />
+			<TiThMenu className={styles.btnMenu} size={20} color={'#333'} />
 		</header>
 	);
 }
