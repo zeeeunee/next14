79ca1369c8a8b5b83de305e70df189368f0bfb70
zeeeunee <wldmstv@gmail.com>
2024-01-15 16:15:16 +0900

    header 스타일링 완료

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,8 @@
+import clsx from 'clsx';
 import styles from './about.module.scss';
 
 export default function About() {
-	throw new Error('Error in About page..');
+	// throw new Error('Error in About page..');
 	return (
 		<section className={clsx(styles.about)}>
 			<h1>About</h1>
