0351613f720f040efdc89108e6107db76d1e8a29
zeeeunee <wldmstv@gmail.com>
2024-01-15 12:18:41 +0900

    모든 sass파일에서 variables.scss파일을 전역으로 읽을수 있도록 서버쪽에서 해당 import sass구문 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+const nextConfig = {
+	reactStrictMode: false,
+	sassOptions: {
+		includePath: [path.join(__dirname, 'styles')],
+		prependData: `@import '@/styles/variables.scss';`,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
