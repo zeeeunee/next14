644ff7a9c5048f50da9f13b3104059f88ee32256
zeeeunee <wldmstv@gmail.com>
2024-01-15 15:10:38 +0900

    useText훅에 capitalize 추가

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -2,10 +2,6 @@ import clsx from 'clsx';
 import styles from './header.module.scss';
 import Link from 'next/link';
 import Navbar from '../navbar/Navbar';
-import dynamic from 'next/dynamic';
-
-// 기존 clinet 방식의 컴포넌트를 import 시 ssr: false를 통해 서버쪽에서 pre-build되지 않도록 설정
-const NoSsrNavbar = dynamic(() => import('@/components/navbar/Navbar'), { ssr: false });
 
 export default function Header() {
 	console.log('header');
@@ -15,8 +11,7 @@ export default function Header() {
 				<Link href='/'>DCODELAB</Link>
 			</h1>
 
-			{/* <Navbar textArr={['about', 'youtube', 'post']} /> */}
-			<NoSsrNavbar textArr={['about', 'youtube', 'post']} />
+			<Navbar textArr={['about', 'youtube', 'post']} />
 		</header>
 	);
 }
