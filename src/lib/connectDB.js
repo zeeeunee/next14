0776d62ee4d97d18136a1d244caeea71e0f1e444
zeeeunee <wldmstv@gmail.com>
2024-01-16 15:24:25 +0900

    서버응답함수 생성

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -7,15 +7,14 @@
 
 const mongoose = require('mongoose');
 let isConnected = false;
-
 export const connectDB = async () => {
 	try {
 		if (isConnected) {
 			console.log('already connected!');
 			return;
 		}
-		await mongoose.connect(process.env.MONGO_URL);
-		isConnected = db.connection[0].readyState;
+		const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URL);
+		isConnected = db.connections[0].readyState;
 	} catch (err) {
 		console.log(err);
 		throw new Error('Fail to connect DB!');
