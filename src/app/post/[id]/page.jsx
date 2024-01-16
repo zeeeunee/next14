d166bd52cb33084baa7b64f26da85488b88814e6
zeeeunee <wldmstv@gmail.com>
2024-01-16 16:44:22 +0900

    post상세페이지 스타일링

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,13 +1,22 @@
 import clsx from 'clsx';
-import styles from './postdetail.module.scss';
+import styles from './postDetail.module.scss';
+import { getPosts } from '@/lib/actions';
+import Image from 'next/image';
 
 export default async function PostDetail({ params }) {
 	const { id } = params;
 	const post = await getPosts(id);
-	console.log(post);
 	return (
-		<section className={clsx(styles.postdetail)}>
-			<h1>Postdetail</h1>
+		<section className={clsx(styles.postDetail)}>
+			<h1>PostDetail</h1>
+
+			<article>
+				<div className={clsx(styles.pic)}>{post.img && <Image src={post.img} alt={post.title} priority fill />}</div>
+				<div className={clsx(styles.txt)}>
+					<h2>{post.title}</h2>
+					<p>{post.desc}</p>
+				</div>
+			</article>
 		</section>
 	);
 }
