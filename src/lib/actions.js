11da3277851d3198b85f335f8e8a4d71a8a5efb8
zeeeunee <wldmstv@gmail.com>
2024-01-17 12:12:41 +0900

    서버쪽에서 서버액션을 통해 글삭제 이벤트 연결

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -37,5 +37,20 @@ export const addPost = async formData => {
 
 export const deletePost = async formData => {
 	'use server';
-	console.log(formData);
+
+	try {
+		connectDB();
+		const data = Object.fromEntries(formData);
+		//const id = { _id: Object.keys(data)[0] };
+		const id = Object.keys(data)[0];
+
+		//findByAndDelete(id); id:삭제할 document의 _id의 value값 전달 (객체전달 아님)
+		await Post.findByIdAndDelete(id);
+	} catch (err) {
+		console.log(err);
+		throw new Error('Fail to delete Post');
+	}
+
+	revalidatePath('/post');
+	redirect('/post');
 };
