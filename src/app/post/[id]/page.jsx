b5582d0a39d4bc45c027d3437268ef92659214ed
zeeeunee <wldmstv@gmail.com>
2024-01-17 13:04:31 +0900

    수정

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -10,21 +10,20 @@ export default async function PostDetail({ params }) {
 	return (
 		<section className={clsx(styles.postDetail)}>
 			<h1>PostDetail</h1>
-
 			<article>
 				<div className={clsx(styles.pic)}>{post.img && <Image src={post.img} alt={post.title} priority fill />}</div>
 				<div className={clsx(styles.txt)}>
 					<h2>{post.title}</h2>
 					<p>{post.desc}</p>
-					<Link href='/post/edit'>Edit</Link>
-					{/* 서버컴포넌트에서 onClick 연결 불가(Client방식에서만 가능) */}
-					{/* 이벤트발생시킬 버튼을 form으로 감싼뒤 서버액션함수를 action에 등록, 인수전달시에는 hidden타입으로 input만들어서 name에 연동해놓으면 서버액션함수에 파라미터로 전달됨 */}
-					<form action={deletePost}>
-						<nav>
-							<input type='hidden' name={id} />
-							<button>Delete</button>
-						</nav>
-					</form>
+					<nav>
+						<Link href={`/post/edit/${id}`}>Edit</Link>
+						<form action={deletePost}>
+							<nav>
+								<input type='hidden' name={id} />
+								<button>Delete</button>
+							</nav>
+						</form>
+					</nav>
 				</div>
 			</article>
 		</section>
