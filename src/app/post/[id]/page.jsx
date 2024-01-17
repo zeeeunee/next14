788df5ad681042c2686a32dffff7b54c4f174e82
zeeeunee <wldmstv@gmail.com>
2024-01-17 12:45:49 +0900

    post상세페이지에서 edit버튼 클릭시 edit전용페이지로 이동

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import styles from './postDetail.module.scss';
 import { deletePost, getPosts } from '@/lib/actions';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function PostDetail({ params }) {
 	const { id } = params;
@@ -15,6 +16,7 @@ export default async function PostDetail({ params }) {
 				<div className={clsx(styles.txt)}>
 					<h2>{post.title}</h2>
 					<p>{post.desc}</p>
+					<Link href='/post/edit'>Edit</Link>
 					{/* 서버컴포넌트에서 onClick 연결 불가(Client방식에서만 가능) */}
 					{/* 이벤트발생시킬 버튼을 form으로 감싼뒤 서버액션함수를 action에 등록, 인수전달시에는 hidden타입으로 input만들어서 name에 연동해놓으면 서버액션함수에 파라미터로 전달됨 */}
 					<form action={deletePost}>
