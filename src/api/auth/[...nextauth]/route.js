55cd546be1c5b5581afc5389121b66e69b3e2f79
zeeeunee <wldmstv@gmail.com>
2024-01-18 14:32:21 +0900

    기본 인증 api라우터 설정

--- src/api/auth/[...nextauth]/route.js
+++ src/api/auth/[...nextauth]/route.js
@@ -1,4 +1,31 @@
 //서버에서 모델 데이터 생성을 api라우터에서 처리시에는 해당 api route 에서 get, post 요청에 대한 session응답 처리 해주는게 맞지만
 //현재 프로젝트 구조에서는 모든 서버 데이터를 api라우터가 아닌 lib폴더 안쪽에 server action.js에서 처리하고 있기 때문에
 //프로젝트의 효율적인 관리를 위해 lib폴더 안쪽에 auth.js 에서 get, post요청에 대한 서버 응답 함수를 만들어서 export 한 내용을 /api 라우터에서 import 하자마자 바로 export 처리
-export { GET, POST } from '@/lib/auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth from 'next-auth/next';
+
+export const authOptions = {
+	pages: {
+		signIn: '/login'
+	},
+	providers: [
+		CredentialsProvider({
+			name: 'credentials',
+			credentials: {},
+			async authorize(credentials) {
+				try {
+					const user = await login(credentials);
+					console.log('this user is=', user);
+					return user;
+				} catch (err) {
+					console.log('Error =', err);
+					return null;
+				}
+			}
+		})
+	]
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
