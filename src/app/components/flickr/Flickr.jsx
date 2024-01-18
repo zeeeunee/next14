f08f99b0c5af892f6975f3a4c97c3d16f59ea6a2
zeeeunee <wldmstv@gmail.com>
2024-01-18 11:43:43 +0900

    next-auth를 통한 Authentication 흐름

--- src/app/components/flickr/Flickr.jsx
+++ src/app/components/flickr/Flickr.jsx
@@ -3,27 +3,38 @@
 import clsx from 'clsx';
 import styles from './flickr.module.scss';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useGlobalData } from '@/hooks/useGlobalData';
 
 export default function Flickr() {
+	const inputEl = useRef(null);
+
 	const { ImgPanelOpen, setImgPanelOpen, setImgUrl } = useGlobalData();
 	const [Pics, setPics] = useState([]);
 
-	useEffect(() => {
-		const fetchFlickr = async () => {
-			const num = 21;
-			const flickr_api = process.env.NEXT_PUBLIC_FLICKR_API;
-			const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
-			const method_interest = 'flickr.interestingness.getList';
-			const interestURL = `${baseURL}${method_interest}`;
-			const data = await fetch(interestURL);
-			const response = await data.json();
-			console.log(response.photos.photo);
-			setPics(response.photos.photo);
-		};
+	const fetchFlickr = async opt => {
+		const num = 21;
+		const flickr_api = process.env.NEXT_PUBLIC_FLICKR_API;
+		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
+		const method_interest = 'flickr.interestingness.getList';
+		const method_search = 'flickr.photos.search';
+		let url = '';
+		if (opt.type === 'interest') url = `${baseURL}${method_interest}`;
+		if (opt.type === 'search') url = `${baseURL}${method_search}&tags=${opt.tag}`;
+		const data = await fetch(url);
+		const response = await data.json();
+		setPics(response.photos.photo);
+	};
 
-		fetchFlickr();
+	const handleSearch = e => {
+		e.preventDefault();
+		const tag = inputEl.current.value.trim();
+		if (!tag) return;
+		fetchFlickr({ type: 'search', tag: tag });
+	};
+
+	useEffect(() => {
+		fetchFlickr({ type: 'interest' });
 	}, []);
 
 	return (
@@ -32,6 +43,10 @@ export default function Flickr() {
 				<aside className={clsx(styles.flickr)}>
 					<h1>Flickr</h1>
 					<button onClick={() => setImgPanelOpen(false)}>close</button>
+					<form>
+						<input type='text' placeholder='serach' ref={inputEl} />
+						<button onClick={handleSearch}>Search</button>
+					</form>
 					<div>
 						{Pics.map((pic, idx) => {
 							return (
