32cd477d457a3cd7fec19a57c8d4a2fc00ad704f
zeeeunee <wldmstv@gmail.com>
2024-01-22 12:00:44 +0900

    이미지 업로드시 uploadingthing서버쪽에 이미지파일 업로드 완료및 url값 브라우저 콘솔에서 확인

--- src/app/post/write/page.jsx
+++ src/app/post/write/page.jsx
@@ -3,6 +3,7 @@ import styles from './write.module.scss';
 import { addPost } from '@/lib/actions';
 import Flickr from '@/components/flickr/Flickr';
 import InputImage from '@/components/inputImage/InputImage';
+import UploadImage from '@/components/uploadImage/UploadImage';
 import { auth } from '@/lib/auth';
 
 export default async function Write() {
@@ -18,6 +19,7 @@ export default async function Write() {
 					<input type='hidden' name='email' defaultValue={session.user.email} />
 					<input type='text' placeholder='title' name='title' />
 					<InputImage />
+					<UploadImage />
 					<textarea name='desc' cols='30' rows='3' placeholder='description'></textarea>
 					<nav>
 						<input type='reset' value='cancel' />
