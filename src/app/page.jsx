e2c818b0a7965c80fa6d09092be9a4e92084a15c
zeeeunee <wldmstv@gmail.com>
2024-01-23 09:50:34 +0900

    메인페이지 글자를 Main Page로 바꾸기

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from './main.module.scss';
 export default function Home() {
 	return (
 		<main className={clsx(styles.main)}>
-			<h1>메인페이지</h1>
+			<h1>Main Page</h1>
 		</main>
 	);
 }
