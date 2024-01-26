1d65d57ffa008ba9f1bb88522a88bbaccc00e2b5
zeeeunee <wldmstv@gmail.com>
2024-01-26 09:55:47 +0900

    각 페이지별 로딩처리

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,6 @@ import Header from '@/components/header/Header';
 import { GlobalProvider } from '@/hooks/useGlobalData';
 import MobileMenu from '@/components/mobileMenu/MobileMenu';
 import '@uploadthing/react/styles.css';
-import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +18,6 @@ export default function RootLayout({ children }) {
 				<GlobalProvider>
 					<main className='container'>
 						<Header />
-
 						<div className='wrap'>{children}</div>
 					</main>
 					<MobileMenu />
