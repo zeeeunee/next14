aeea87689800541defe58cfee2baf55673f71edf
zeeeunee <wldmstv@gmail.com>
2024-01-26 09:51:06 +0900

    글작성 버튼 클릭시 서버액션 실행동안 로딩바 출력

--- src/app/post/write/page.jsx
+++ src/app/post/write/page.jsx
@@ -1,18 +1,17 @@
 import clsx from 'clsx';
-
+import styles from './write.module.scss';
 import { addPost } from '@/lib/actions';
 import InputImage from '@/components/inputImage/InputImage';
 import { auth } from '@/lib/auth';
 import ImageUploader from '@/components/uploadImage/UploadImage';
+import BtnSubmit from '@/components/btnSubmit/BtnSubmit';
 
 export default async function Write() {
 	const session = await auth();
-
 	return (
 		<>
 			<section className={clsx(styles.write)}>
 				<h1>Write Post</h1>
-
 				<form action={addPost}>
 					<input type='hidden' name='email' defaultValue={session.user.email} />
 					<input type='text' placeholder='title' name='title' />
@@ -22,7 +21,8 @@ export default async function Write() {
 
 					<nav>
 						<input type='reset' value='cancel' />
-						<input type='submit' value='write' />
+						{/* <input type='submit' value='write' /> */}
+						<BtnSubmit />
 					</nav>
 				</form>
 			</section>
